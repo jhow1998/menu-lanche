c17f1ba9689df1110cec51ba15634e73cd6f8dd3
joseph <joseph.robert.carvalho@gmail.com>
2022-09-28 18:44:41 -0300

    version v1.0.0

--- js/script.js
+++ js/script.js
@@ -1,25 +1,61 @@
-var swiper = new Swiper(".mySwiper", {
+
+  let searchBtn = document.querySelector('#search-btn');
+  let searchForm = document.querySelector('.header .search-form');
+  //evento para abrir a barra de pesquisa
+  searchBtn.onclick = () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchForm.classList.toggle('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+  }
+
+ 
+  let menu = document.querySelector("#menu-btn");
+  let navbar = document.querySelector('.header .navbar');
+ //evento para abrir o navbar
+  menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+    
+  };
+  // variaveis do modal
+  let loginForm = document.querySelector('.entrega-container');
+  let formClose = document.querySelector('#close');
+  let formBtn = document.querySelector('#cep-btn');
+  // evento para abrir o modal
+  formBtn.addEventListener('click',()=> {
+    loginForm.classList.add('active');
+  });
+  //evento para fecha o modal
+  formClose.addEventListener('click',()=>{
+    loginForm.classList.remove('active');
+  })
+
+  //objeto da bliblioteca js swiper
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
     pagination: {
         el: ".swiper-pagination",
      },
     breakpoints: {
+      340: {
+        slidesPerView: 4,
+      },
         540: {
           slidesPerView: 5,
         },
         768: {
-          slidesPerView: 4,
+          slidesPerView: 6,
         },
         1024: {
-          slidesPerView: 6,
+          slidesPerView: 8,
         },
     },
   });
 
-  let menu = document.querySelector("#menu-btn");
-  let navbar = document.querySelector('.header .navbar');
   
-  menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-  };
+
+  
   
